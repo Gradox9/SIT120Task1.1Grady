128e8c5759d547518b8af8255be752fb8d9330f9
Gradox9 <63984286+Gradox9@users.noreply.github.com>
2023-08-24 10:46:47 +1000

    Added about and portfolio content

--- MyPortfolio/my-porfolio/src/views/About.vue
+++ MyPortfolio/my-porfolio/src/views/About.vue
@@ -1,5 +1,40 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About me</h1>
   </div>
+
+  <div class="container">
+    <table id="aboutsection">
+      <tr>
+        <td class="worktext">
+          <p class="abouttile">
+            I'm Grady, a undergraduate studying for a Bachelor's of IT student
+            at deakin university.
+          </p>
+          <p class="aboutdescription">
+            Hey there, I'm Grady, a student based in geelong with a joy for web
+            design and network engineering. I enjoy working on design operations
+            and systems that help bring order to complexity. When i'm not
+            working and have free time, you'll probably find me playing a varity
+            of games whilst socialising online with my mates and in the company
+            of my cat Babushka. Here's a photo of her.
+          </p>
+        </td>
+        <td class="worktext">
+          <img
+            class="cat"
+            src="../../public/images/babushka-sad.jpg"
+          />
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <div class="resume-button">
+            <a href="../../public/resume/Resume1.1.docx" download>Download Resume</a>
+          </div>
+        </td>
+      </tr>
+    </table>
+  </div>
+
 </template>
