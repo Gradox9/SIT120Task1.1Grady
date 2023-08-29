d25aee9a84a515a030ff6577b83d499da7f7fda2
Gradox9 <63984286+Gradox9@users.noreply.github.com>
2023-08-29 12:36:36 +1000

    Added contact but broke portfolio

--- MyPortfolio/my-porfolio/src/App.vue
+++ MyPortfolio/my-porfolio/src/App.vue
@@ -2,7 +2,8 @@
   <div id="nav">
     <router-link to="/">Home</router-link>
     <router-link to="/portfolio">Portfolio</router-link>
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About Me</router-link>
+    <router-link to="/contact">Contact Me</router-link>
   </div>
   <div class="container">
     <router-view />
--- MyPortfolio/my-porfolio/src/router/index.js
+++ MyPortfolio/my-porfolio/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import { compile } from 'vue'
 import Portfolio from '../views/Portfolio.vue'
+import Contact from '../views/Contact Me.vue'
 
 const routes = [
   {
@@ -21,6 +22,10 @@ const routes = [
     path: '/portfolio',
     name: 'portfolio',
     component: Portfolio,
+
+    path: '/contact',
+    name: 'contact',
+    component: Contact,
   }
 ]
 
